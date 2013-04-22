fc75c88d4e7e911fb5442c6076bd8ca84f60a8f4
liang8305 <418094911@qq.com>
2013-04-22 10:24:47 +0800

    add ssh short cut

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -1,21 +1,25 @@
 function zsh_stats() {
-  history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20
+history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20
 }
 
 function uninstall_oh_my_zsh() {
-  /usr/bin/env ZSH=$ZSH /bin/sh $ZSH/tools/uninstall.sh
+/usr/bin/env ZSH=$ZSH /bin/sh $ZSH/tools/uninstall.sh
 }
 
 function upgrade_oh_my_zsh() {
-  /usr/bin/env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
+/usr/bin/env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
 }
 
 function take() {
-  mkdir -p $1
-  cd $1
+mkdir -p $1
+cd $1
 }
 
-#function ssh(){
-#	ssh $@
-#}
+function ssh(){
+if [[ "$1" =~ "^0[0-9]{1}$" ]] ;then
+	/usr/bin/ssh "liangwei@yunbd$1.mail.cnb.yahoo.com"
+else
+	/usr/bin/ssh $@
+fi
+}
 
