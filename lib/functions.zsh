e375d82d8a91cfa2e6f863a5bbe3a198c630be80
liang8305 <418094911@qq.com>
2013-05-15 14:50:38 +0800

    ssh s1 s2 server shortcut

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -18,6 +18,10 @@ cd $1
 function ssh(){
 if [[ "$1" =~ "^0[0-9]{1}$" ]] ;then
 	/usr/bin/ssh "liangwei@yunbd$1.mail.cnb.yahoo.com"
+elif [[ "$1" =~ "^s1$" ]] ;then
+	/usr/bin/ssh "liangwei@125.208.1.22" -p7888
+elif [[ "$1" =~ "^s2$" ]] ;then
+	/usr/bin/ssh "liangwei@125.208.1.21" -p7888
 else
 	/usr/bin/ssh $@
 fi
