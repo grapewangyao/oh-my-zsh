b9505f14e1adb6176983bf345062fc03b9285154
liang8305 <418094911@qq.com>
2013-06-28 10:34:55 +0800

    a

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,6 +1,5 @@
 printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
 cd "$ZSH"
-<<<<<<< HEAD
 
 git remote add robbyrussell https://github.com/robbyrussell/oh-my-zsh.git
 git fetch robbyrussell
@@ -8,9 +7,6 @@ git merge robbyrussell/master
 git push origin master
 
 if git pull origin master
-=======
-if git pull --rebase origin master
->>>>>>> robbyrussell/master
 then
 	printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
 	printf '\033[0;32m%s\033[0m\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
